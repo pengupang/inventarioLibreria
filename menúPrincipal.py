5fd00a98c0b0742cc0a111f4f870d5dede8ce9c4
Maciel Martinez <maciel.mz.riquelme@gmail.com>
2024-06-06 17:54:29 -0400

    arreglos en img
    Se obtiene path de la imagen de forma dinamica

--- menúPrincipal.py
+++ menúPrincipal.py
@@ -19,10 +19,18 @@ posicion_x = (ancho_pantalla - ancho_ventana) // 2
 posicion_y = (alto_pantalla - alto_ventana) // 2
 
 ventana.geometry(f"{ancho_ventana}x{alto_ventana}+{posicion_x}+{posicion_y}")
-path_imagen = "D:\escritorio\Taller Desarrollo aplicaciones\inventarioLibreria\img\libro-abierto.png"
-imagen= PhotoImage(file=path_imagen)
 
+#obtiene dir actual
+path_actual = os.getcwd()
+#obtiene el dir de las imagenes en este caso utiliza el path del dir actual
+#ya que las imagenes estan en un dir dentro
+folder_imagen = os.path.join(path_actual,"img")
+#con ambos path juntos busca la imagen
+path_imagen = os.path.join(folder_imagen,"libro-abierto.png")
+#la imagen se agrega a u label para ser mostrada
+imagen= PhotoImage(file=path_imagen)
 iLabel= tk.Label(ventana, image=imagen)
+#se acomoda la imagen 
 iLabel.place(relx=0.5 , rely=0.5,anchor=CENTER)
 ToolBar(ventana)
 
