1d66dfaa8d49649d1f40c108af2a37d45f6eac81
Maciel Martinez <maciel.mz.riquelme@gmail.com>
2024-06-10 16:20:32 -0400

    actulizacion de anotacioens
    se agregaron descripciones a partes del codigo

--- frameVerProductos.py
+++ frameVerProductos.py
@@ -12,7 +12,7 @@ class FrameVerProductos():
         tabla.column("#0",width=0)
         #agrega texto a los headings de las columnas
         tabla.heading("1",text="Titulo")
-        tabla.heading("2",text="autor")
-        tabla.heading("3",text="Titulo")
+        tabla.heading("2",text="Autor")
+        tabla.heading("3",text="Stock")
         #acomoda la tabla en la ventana
         tabla.place(x=325,y=300,anchor="center")
