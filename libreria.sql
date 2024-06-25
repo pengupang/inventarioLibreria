29b3e24524cc887447d8233df32261f67061b64f
TaperLimpio <virus_anta1@hotmail.com>
2024-06-25 18:37:02 -0400

    Base de datos actualizada

--- libreria.sql
+++ libreria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-06-2024 a las 22:31:39
+-- Tiempo de generación: 25-06-2024 a las 05:12:00
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -29,8 +29,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `autor` (
   `ID` int(11) NOT NULL,
-  `Nombre` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `Nombre` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `autor`
@@ -38,50 +38,7 @@ CREATE TABLE `autor` (
 
 INSERT INTO `autor` (`ID`, `Nombre`) VALUES
 (1, 'J.R.R Tolkien'),
-(2, 'Luis Dom?nech');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `autor_libro`
---
-
-CREATE TABLE `autor_libro` (
-  `ID_Autor` int(11) NOT NULL,
-  `ID_Libro` int(11) NOT NULL,
-  `Rol` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `autor_libro`
---
-
-INSERT INTO `autor_libro` (`ID_Autor`, `ID_Libro`, `Rol`) VALUES
-(1, 1, 'Escritor'),
-(1, 2, 'Escritor'),
-(1, 3, 'Escritor'),
-(2, 1, 'Traductor');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `detalle_movimiento`
---
-
-CREATE TABLE `detalle_movimiento` (
-  `ID` int(11) NOT NULL,
-  `Cantidad` int(11) NOT NULL,
-  `ID_Libro` int(11) NOT NULL,
-  `ID_Movimiento` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `detalle_movimiento`
---
-
-INSERT INTO `detalle_movimiento` (`ID`, `Cantidad`, `ID_Libro`, `ID_Movimiento`) VALUES
-(1, 1, 1, 1),
-(2, 1, 2, 2);
+(2, 'Tony DiTerlizzi');
 
 -- --------------------------------------------------------
 
@@ -91,8 +48,8 @@ INSERT INTO `detalle_movimiento` (`ID`, `Cantidad`, `ID_Libro`, `ID_Movimiento`)
 
 CREATE TABLE `editorial` (
   `ID` int(11) NOT NULL,
-  `Nombre` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `Nombre` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `editorial`
@@ -105,46 +62,26 @@ INSERT INTO `editorial` (`ID`, `Nombre`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `editorial_libro`
---
-
-CREATE TABLE `editorial_libro` (
-  `ID_Editorial` int(11) NOT NULL,
-  `ID_Libro` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `editorial_libro`
---
-
-INSERT INTO `editorial_libro` (`ID_Editorial`, `ID_Libro`) VALUES
-(1, 1),
-(1, 2),
-(1, 3),
-(2, 1),
-(2, 2),
-(2, 3);
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `libro`
 --
 
 CREATE TABLE `libro` (
   `ID` int(11) NOT NULL,
-  `Titulo` varchar(60) DEFAULT NULL,
-  `Stock` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `Titulo` varchar(60) NOT NULL,
+  `Stock` int(11) NOT NULL,
+  `ID_Autor` int(11) NOT NULL,
+  `ID_Editorial` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `libro`
 --
 
-INSERT INTO `libro` (`ID`, `Titulo`, `Stock`) VALUES
-(1, 'El se?or de los anillos: La comunidad del anillo', 1),
-(2, 'El se?or de los anillos: Las dos torres', 1),
-(3, 'El se?or de los anillos: El retorno del rey', 1);
+INSERT INTO `libro` (`ID`, `Titulo`, `Stock`, `ID_Autor`, `ID_Editorial`) VALUES
+(1, 'El señor de los anillos: La comunidad del anillo', 1, 1, 1),
+(2, 'El señor de los anillos: Las dos torres', 1, 1, 1),
+(3, 'El señor de los anillos: El retorno del rey', 1, 1, 1),
+(4, 'En busca de Wondla', 2, 2, 2);
 
 -- --------------------------------------------------------
 
@@ -155,18 +92,20 @@ INSERT INTO `libro` (`ID`, `Titulo`, `Stock`) VALUES
 CREATE TABLE `movimiento` (
   `ID` int(11) NOT NULL,
   `ID_Libro` int(11) NOT NULL,
+  `ID_Proveedor` int(11) DEFAULT NULL,
   `ID_Tipo_movimiento` int(11) NOT NULL,
   `Fecha` date NOT NULL,
+  `Cantidad` int(11) NOT NULL,
   `Total_neto` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `movimiento`
 --
 
-INSERT INTO `movimiento` (`ID`, `ID_Libro`, `ID_Tipo_movimiento`, `Fecha`, `Total_neto`) VALUES
-(1, 1, 1, '2024-04-17', 7000),
-(2, 2, 1, '2024-04-17', 7000);
+INSERT INTO `movimiento` (`ID`, `ID_Libro`, `ID_Proveedor`, `ID_Tipo_movimiento`, `Fecha`, `Cantidad`, `Total_neto`) VALUES
+(1, 1, NULL, 1, '2024-04-17', 1, 7000),
+(2, 2, NULL, 2, '2024-04-17', 1, 7000);
 
 -- --------------------------------------------------------
 
@@ -176,8 +115,8 @@ INSERT INTO `movimiento` (`ID`, `ID_Libro`, `ID_Tipo_movimiento`, `Fecha`, `Tota
 
 CREATE TABLE `proveedor` (
   `ID` int(11) NOT NULL,
-  `Nombre` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `Nombre` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `proveedor`
@@ -196,8 +135,8 @@ INSERT INTO `proveedor` (`ID`, `Nombre`) VALUES
 
 CREATE TABLE `tipo_movimiento` (
   `ID` int(11) NOT NULL,
-  `Tipo` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `Tipo` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `tipo_movimiento`
@@ -215,9 +154,9 @@ INSERT INTO `tipo_movimiento` (`ID`, `Tipo`) VALUES
 
 CREATE TABLE `usuario` (
   `ID` int(11) NOT NULL,
-  `Nombre` varchar(40) NOT NULL,
-  `Password` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `Nombre` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `Password` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `usuario`
@@ -237,32 +176,10 @@ ALTER TABLE `autor`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indices de la tabla `autor_libro`
---
-ALTER TABLE `autor_libro`
-  ADD PRIMARY KEY (`ID_Autor`,`ID_Libro`),
-  ADD KEY `ID_Libro` (`ID_Libro`);
-
---
--- Indices de la tabla `detalle_movimiento`
---
-ALTER TABLE `detalle_movimiento`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `ID_Libro` (`ID_Libro`),
-  ADD KEY `ID_Movimiento` (`ID_Movimiento`);
-
---
 -- Indices de la tabla `editorial`
 --
 ALTER TABLE `editorial`
   ADD PRIMARY KEY (`ID`);
-
---
--- Indices de la tabla `editorial_libro`
---
-ALTER TABLE `editorial_libro`
-  ADD PRIMARY KEY (`ID_Editorial`,`ID_Libro`),
-  ADD KEY `ID_Libro` (`ID_Libro`);
 
 --
 -- Indices de la tabla `libro`
@@ -276,7 +193,8 @@ ALTER TABLE `libro`
 ALTER TABLE `movimiento`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `ID_Tipo_movimiento` (`ID_Tipo_movimiento`),
-  ADD KEY `ID_Libro` (`ID_Libro`);
+  ADD KEY `ID_Libro` (`ID_Libro`),
+  ADD KEY `ID_Proveedor` (`ID_Proveedor`);
 
 --
 -- Indices de la tabla `proveedor`
@@ -307,12 +225,6 @@ ALTER TABLE `autor`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT de la tabla `detalle_movimiento`
---
-ALTER TABLE `detalle_movimiento`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
 -- AUTO_INCREMENT de la tabla `editorial`
 --
 ALTER TABLE `editorial`
@@ -322,7 +234,7 @@ ALTER TABLE `editorial`
 -- AUTO_INCREMENT de la tabla `libro`
 --
 ALTER TABLE `libro`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `movimiento`
@@ -353,32 +265,12 @@ ALTER TABLE `usuario`
 --
 
 --
--- Filtros para la tabla `autor_libro`
---
-ALTER TABLE `autor_libro`
-  ADD CONSTRAINT `autor_libro_ibfk_1` FOREIGN KEY (`ID_Autor`) REFERENCES `autor` (`ID`),
-  ADD CONSTRAINT `autor_libro_ibfk_2` FOREIGN KEY (`ID_Libro`) REFERENCES `libro` (`ID`);
-
---
--- Filtros para la tabla `detalle_movimiento`
---
-ALTER TABLE `detalle_movimiento`
-  ADD CONSTRAINT `detalle_movimiento_ibfk_1` FOREIGN KEY (`ID_Libro`) REFERENCES `libro` (`ID`),
-  ADD CONSTRAINT `detalle_movimiento_ibfk_2` FOREIGN KEY (`ID_Movimiento`) REFERENCES `movimiento` (`ID`);
-
---
--- Filtros para la tabla `editorial_libro`
---
-ALTER TABLE `editorial_libro`
-  ADD CONSTRAINT `editorial_libro_ibfk_1` FOREIGN KEY (`ID_Editorial`) REFERENCES `editorial` (`ID`),
-  ADD CONSTRAINT `editorial_libro_ibfk_2` FOREIGN KEY (`ID_Libro`) REFERENCES `libro` (`ID`);
-
---
 -- Filtros para la tabla `movimiento`
 --
 ALTER TABLE `movimiento`
   ADD CONSTRAINT `movimiento_ibfk_1` FOREIGN KEY (`ID_Tipo_movimiento`) REFERENCES `tipo_movimiento` (`ID`),
-  ADD CONSTRAINT `movimiento_ibfk_2` FOREIGN KEY (`ID_Libro`) REFERENCES `libro` (`ID`);
+  ADD CONSTRAINT `movimiento_ibfk_2` FOREIGN KEY (`ID_Libro`) REFERENCES `libro` (`ID`),
+  ADD CONSTRAINT `movimiento_ibfk_3` FOREIGN KEY (`ID_Proveedor`) REFERENCES `proveedor` (`ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
