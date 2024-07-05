6359bacf90bb5dfdd415f2aa7126bbeb2db37742
Maciel Martinez <maciel.mz.riquelme@gmail.com>
2024-07-05 19:05:28 -0400

    cambios visuales toolbar
    se cambio un poco la apariencia de la toolbar

--- Vista/toolBar.py
+++ Vista/toolBar.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import Frame
 
+
 class ToolBar(Frame):
     def __init__(self, master, callback):
         super().__init__(master)
@@ -11,33 +12,33 @@ class ToolBar(Frame):
 
     def crear_botones(self):
         # Crear el botón Autores
-        autores = tk.Button(self.menu_frame, text="Autores", background="gray", foreground="white",
-                            activeforeground="black", activebackground="gray52", command=lambda: self.enviar_senal("VistaAutores"))
+        autores = tk.Button(self.menu_frame, text="Autores", background="gray", foreground="white",bd=0,
+                            activeforeground="black", activebackground="gray52", command=lambda: self.enviar_señal("VistaAutores"))
         autores.pack(side="left", padx=2, pady=2)
 
           # Crear el botón Editorial
-        editorial = tk.Button(self.menu_frame, text="Editorial", background="gray", foreground="white",
-                               activeforeground="black", activebackground="gray52", command=lambda: self.enviar_senal("VistaEditorial"))
+        editorial = tk.Button(self.menu_frame, text="Editorial", background="gray", foreground="white",bd=0,
+                               activeforeground="black", activebackground="gray52", command=lambda: self.enviar_señal("VistaEditorial"))
         editorial.pack(side="left", padx=2, pady=2)
 
         # Crear el botón Movimientos
-        Movimientos = tk.Button(self.menu_frame, text="Movimientos", background="gray", foreground="white",
-                           activeforeground="black", activebackground="gray52", command=lambda: self.enviar_senal("VistaMovimientos"))
+        Movimientos = tk.Button(self.menu_frame, text="Movimientos", background="gray", foreground="white",bd=0,
+                           activeforeground="black", activebackground="gray52", command=lambda: self.enviar_señal("VistaMovimientos"))
         Movimientos.pack(side="left", padx=2, pady=2)
 
         # Crear el botón Productos
-        productos = tk.Button(self.menu_frame, text="Productos", background="gray", foreground="white",
-                              activeforeground="black", activebackground="gray52", command=lambda: self.enviar_senal("VistaProductos"))
+        productos = tk.Button(self.menu_frame, text="Productos", background="gray", foreground="white",bd=0,
+                              activeforeground="black", activebackground="gray52", command=lambda: self.enviar_señal("VistaProductos"))
         productos.pack(side="left", padx=2, pady=2)
 
         # Crear el botón Proveedores
-        proveedores = tk.Button(self.menu_frame, text="Proveedores", background="gray", foreground="white",
-                                activeforeground="black", activebackground="gray52", command=lambda: self.enviar_senal("VistaProveedores"))
+        proveedores = tk.Button(self.menu_frame, text="Proveedores", background="gray", foreground="white",bd=0,
+                                activeforeground="black", activebackground="gray52", command=lambda: self.enviar_señal("VistaProveedores"))
         proveedores.pack(side="left", padx=2, pady=2)
 
         
 
         
 
-    def enviar_senal(self, senal):
-        self.callback(senal)
+    def enviar_señal(self, señal):
+        self.callback(señal)
