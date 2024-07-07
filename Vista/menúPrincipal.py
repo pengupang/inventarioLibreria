523be992949b4be241f593d1c8717a3c47293477
Maciel Martinez <maciel.mz.riquelme@gmail.com>
2024-07-07 16:57:13 -0400

    Messagebox
    Se agregó messagebox de pregunta al insertar datos
    Ademas de uno al cerrar la aplicacion

--- Vista/menúPrincipal.py
+++ Vista/menúPrincipal.py
@@ -1,5 +1,6 @@
-import os
+
 from customtkinter import *
+from tkinter import messagebox
 
 # En esta clase Es donde aparecen todos los frames
 #Es el contenedor pricipal
@@ -18,3 +19,11 @@ class VentanaPrincipal (CTk):
         self.config(background="#dbdbdb")
         self.main_frame = None
 
+        self.protocol("WM_DELETE_WINDOW",self.cerrarVentana)
+
+    def cerrarVentana(self):
+        cerrar= messagebox.askyesno(message="Está seguro que quiere cerrar la aplicación?", title= "Confirmar cierre")
+
+        if cerrar:
+            self.destroy()
+    
